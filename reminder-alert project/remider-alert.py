bb29de4873386634cde24ed6e4b8821332102597
Joan Ferley Mosquera Lozano <mosqueralozanojhoanferley@gmail.com>
2022-03-05 08:05:02 -0500

    added the reminder-alert project

---
+++ reminder-alert project/remider-alert.py
@@ -0,0 +1,11 @@
+from tkinter import messagebox
+import time
+from playsound import playsound
+
+# This is created to remind this punk to check his email
+
+while True:
+
+    time.sleep(300)
+    playsound("rigtone.mp3")
+    messagebox.showinfo("Alert!!", "Check you motherfuking email punk!! Maybe you have work")
